35991218d93dcc93c7ffd4c6f20cf1e24aa3434b
Gunasekhar Matamalam <gunasekhar.matamalam@suse.com>
2024-06-17 07:55:01 -0700

    Added tabs.sh to convert Docusaurus tabs to AsciiDoc tabs

--- convert.sh
+++ convert.sh
@@ -10,6 +10,7 @@ cp -r ./i18n/zh/docusaurus-plugin-content-docs/current ./kramdown_md_to_asciidoc
 
 find ./kramdown_md_to_asciidoc -type f \( -name "*.md" -o -name "*.mdx" \) -exec sh -c 'echo Processing Head tag in file $1 & head_tag.sh "$1" "antora"' _ {} \;
 find ./kramdown_md_to_asciidoc -type f -name "*.md" -exec sh -c 'echo Replacing Collapsible blocks in file $1 & collapsible_block.sh "$1" ' _ {} \;
+find ./kramdown_md_to_asciidoc -type f -name "*.md" -exec sh -c 'echo Replacing Tabs in file $1 & tabs.sh "$1" ' _ {} \;
 find ./kramdown_md_to_asciidoc -type f -name "*.md" -exec sh -c 'echo Converting file $1 & kramdoc -o "${1%.md}.adoc" "$1"' _ {} \;
 find ./kramdown_md_to_asciidoc -type f -name "*.adoc" -exec sh -c 'echo Replacing Admonitions in file $1 & admon.sh "$1" ' _ {} \;
 find ./kramdown_md_to_asciidoc -type f -name "*.adoc" -exec sh -c 'echo Replacing cross reference links in file "$1" && sed -i "s|\\(link:[^ ]*\\)\\.md|\\1.adoc|g" "$1"' _ {} \;
